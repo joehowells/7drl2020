137ff7f48a4389859433d8d30f914c353e20fec7
joehowells <37474737+joehowells@users.noreply.github.com>
2020-03-01 10:21:49 +0000

    Add build script

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="7drl2020",
+    executables=[Executable("main.py", base=base)]
+)
