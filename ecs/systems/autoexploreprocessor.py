e309120545a8def4188c930988474a885e2e5c2f
joehowells <37474737+joehowells@users.noreply.github.com>
2020-03-02 12:38:23 +0000

    Only recalculate auto-explore mapping when new tiles explored

--- ecs/systems/autoexploreprocessor.py
+++ ecs/systems/autoexploreprocessor.py
@@ -6,11 +6,17 @@ from esper import Processor
 from ecs.components.map import Map
 from ecs.components.player import Player
 from ecs.components.position import Position
+from ecs.eventmixin import EventMixin
 from functions import dijkstra_map, iter_neighbors
 
 
-class AutoExploreProcessor(Processor):
+class AutoExploreProcessor(Processor, EventMixin):
     def process(self):
+        event = self.get_event("new_tiles_explored")
+
+        if not event:
+            return
+
         _, map_ = next(iter(self.world.get_component(Map)))
         _, (_, position) = next(iter(self.world.get_components(Player, Position)))
 
--- ecs/systems/visionprocessor.py
+++ ecs/systems/visionprocessor.py
@@ -2,13 +2,15 @@ import itertools
 
 from esper import Processor
 
+from ecs.components.event import Event
 from ecs.components.map import Map
 from ecs.components.player import Player
 from ecs.components.position import Position
+from ecs.eventmixin import EventMixin
 from functions import line_iter
 
 
-class VisionProcessor(Processor):
+class VisionProcessor(Processor, EventMixin):
     def process(self):
         _, map_ = next(iter(self.world.get_component(Map)))
 
@@ -24,10 +26,18 @@ class VisionProcessor(Processor):
 
         map_.visible[position.y][position.x] = True
         map_.explored[position.y][position.x] = True
+        new_tiles_explored = False
 
         for x, y in itertools.product(range(x_min, x_max), range(y_min, y_max)):
             for xi, yi in line_iter(position.x, position.y, x, y):
                 map_.visible[yi][xi] = True
-                map_.explored[yi][xi] = True
+
+                if not map_.explored[yi][xi]:
+                    map_.explored[yi][xi] = True
+                    new_tiles_explored = True
+
                 if not map_.transparent[yi][xi]:
                     break
+
+        if new_tiles_explored:
+            self.set_event(Event("new_tiles_explored", {}))
