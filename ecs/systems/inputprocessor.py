c32f66238199208d29fc05513e042025a436ea1a
joehowells <37474737+joehowells@users.noreply.github.com>
2020-03-03 11:59:11 +0000

    Ignore invalid input

--- ecs/systems/inputprocessor.py
+++ ecs/systems/inputprocessor.py
@@ -1,27 +1,32 @@
 from bearlibterminal import terminal
 from esper import Processor
 
+from ecs.components.dead import Dead
 from ecs.components.player import Player
 from ecs.eventmixin import EventMixin
 
 
 class InputProcessor(Processor, EventMixin):
     def process(self):
-        event = terminal.read()
+        while True:
+            event = terminal.read()
 
-        while terminal.has_input():
-            terminal.read()
+            while terminal.has_input():
+                terminal.read()
 
-        if event == terminal.TK_CLOSE:
-            raise SystemExit
+            if event == terminal.TK_CLOSE:
+                raise SystemExit
 
-        _, player = next(iter(self.world.get_component(Player)))
-        player.action = player.attack_action
+            entity, player = next(iter(self.world.get_component(Player)))
 
-        if event == terminal.TK_Z:
-            player.action = player.attack_action
-            player.anger = min(max(player.anger + player.action.data.get("anger", 0), 0), 100)
+            if not self.world.has_component(entity, Dead):
+                if event == terminal.TK_Z:
+                    player.action = player.attack_action
+                    player.anger = min(max(player.anger + player.action.data.get("anger", 0), 0), 100)
+                    return
 
-        if event == terminal.TK_X:
-            player.action = player.defend_action
-            player.anger = min(max(player.anger + player.action.data.get("anger", 0), 0), 100)
+                if event == terminal.TK_X:
+                    _, player = next(iter(self.world.get_component(Player)))
+                    player.action = player.defend_action
+                    player.anger = min(max(player.anger + player.action.data.get("anger", 0), 0), 100)
+                    return
