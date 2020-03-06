4e0aae8efce21a105b9ef6ab2fe74c2b3a69f53d
joehowells <37474737+joehowells@users.noreply.github.com>
2020-03-06 12:36:04 +0000

    Calculate visible threat separately from monster attacks

--- ecs/processors/threatprocessor.py
+++ ecs/processors/threatprocessor.py
@@ -2,18 +2,22 @@ from random import randint, choices
 
 from esper import Processor
 
+from constants import DijkstraMap
 from ecs.components.assassin import Assassin
 from ecs.components.blinded import Blinded
 from ecs.components.dead import Dead
+from ecs.components.map import Map
 from ecs.components.message import Message
 from ecs.components.monster import Monster
 from ecs.components.player import Player
+from ecs.components.position import Position
 from ecs.components.threatening import Threatening
 from ecs.components.visible import Visible
 
 
 class ThreatProcessor(Processor):
     def process(self):
+        _, game_map = next(iter(self.world.get_component(Map)))
         player_entity, player = next(iter(self.world.get_component(Player)))
 
         player.visible_threat = 0
@@ -22,7 +26,7 @@ class ThreatProcessor(Processor):
         monsters = []
         weights = []
 
-        for entity, (monster, visible) in self.world.get_components(Monster, Visible):
+        for entity, (monster, visible, position) in self.world.get_components(Monster, Visible, Position):
             if self.world.has_component(entity, Blinded):
                 continue
 
@@ -30,17 +34,21 @@ class ThreatProcessor(Processor):
                 threat = max(0, max(monster.threat) - player.defend)
                 player.visible_threat += threat
 
-            for threatening in self.world.try_component(entity, Threatening):
-                threat = max(0, threatening.threat - player.defend)
+            distance = game_map.dijkstra[DijkstraMap.PLAYER][position.y][position.x]
+            if 1 <= distance <= len(monster.threat):
+                threat = monster.threat[distance - 1]
+                threat = max(0, threat - player.defend)
                 player.actual_threat += threat
 
+            for threatening in self.world.try_component(entity, Threatening):
+                threat = max(0, threatening.threat - player.defend)
                 monsters.append(monster)
                 weights.append(threat)
 
         player.visible_threat = min(max(player.visible_threat, 0), 20)
         player.actual_threat = min(max(player.actual_threat, 0), 20)
 
-        if player.actual_threat == 0:
+        if not monsters or not weights:
             return
 
         monster = choices(monsters, weights)[0]
